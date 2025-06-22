e30dab18ca929f4ddea49083ac3c5597fac940b5
Yair <yairdelariva10@gmail.com>
2025-06-22 10:20:27 -0600

    Boton de regresar en informacion de detalles

--- app/src/main/java/com/example/proyecto_bibloteca/ReservacionInfoActivity.kt
+++ app/src/main/java/com/example/proyecto_bibloteca/ReservacionInfoActivity.kt
@@ -1,5 +1,7 @@
 package com.example.proyecto_bibloteca
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +11,7 @@ class ReservacionInfoActivity : AppCompatActivity() {
 
     private lateinit var dbHelper: SQLiteHelper
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reservacion_info)
@@ -20,6 +23,7 @@ class ReservacionInfoActivity : AppCompatActivity() {
         val tvUbicacion = findViewById<TextView>(R.id.tvUbicacion)
         val tvHora = findViewById<TextView>(R.id.tvHora)
         val btnEliminar = findViewById<Button>(R.id.btnEliminar)
+        val btnRegresar = findViewById<Button>(R.id.btnRegresar2)
 
         // Recibir info desde el intent
         val nombre = intent.getStringExtra("nombre") ?: ""
@@ -40,6 +44,14 @@ class ReservacionInfoActivity : AppCompatActivity() {
             } else {
                 Toast.makeText(this, "No se pudo eliminar", Toast.LENGTH_SHORT).show()
             }
+        }
+
+
+        btnRegresar.setOnClickListener {
+            val intent = Intent(this, HomeEstudianteActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
+            finish() // opcional, por si no quieres que regrese con "Back"
         }
     }
 }
